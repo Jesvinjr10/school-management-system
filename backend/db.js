895088f27cc8aef670370c464a7ae2a9dc35227c
jesvin <jesvinljose10@gmail.com>
2025-06-26 12:29:51 +0530

    env files updates db

--- backend/db.js
+++ backend/db.js
@@ -1,18 +1,21 @@
 require('dotenv').config();
-const mysql = require('mysql2');
-require('dotenv').config();
+const { Pool } = require('pg');
 
-
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST=db4free.net,
-  user: process.env.DB_USER=jesvin,
-  password: process.env.DB_PASSWORD=jesvin2701,
-  database: process.env.DB_NAME=school
+const pool = new Pool({
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+  ssl: { rejectUnauthorized: false } 
 });
 
-connection.connect((err) => {
-  if (err) throw err;
-  console.log('Connected to MySQL');
+pool.connect((err) => {
+  if (err) {
+    console.error('❌ Failed to connect to PostgreSQL:', err);
+  } else {
+    console.log('✅ Connected to PostgreSQL');
+  }
 });
 
-module.exports = connection;
+module.exports = pool;
